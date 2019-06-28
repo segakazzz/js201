1ebf1a0e74a6477c96788ed1bb3ef2ed242cb9fd
Kazue Sasatani <segakazue@gmail.com>
2019-06-28 15:02:24 -0500

    Done 220

--- exercises/220-cities.js
+++ exercises/220-cities.js
@@ -8,8 +8,40 @@
 // and returns a new array containing only those cities whose temperature is
 // cooler than 70 degrees.
 
+function coolCities(cities){
+    let coolCitiesArray = []
+    for(let i=0; i<cities.length; i++){
+        let currentCity = cities[i]
+        if (currentCity.temperature < 70){
+            coolCitiesArray.push(currentCity)
+        }
+    }
+    return coolCitiesArray
+}
+
+console.log(coolCities([
+      { name: 'Los Angeles', temperature: 60.0},
+      { name: 'Atlanta', temperature: 52.0 },
+      { name: 'Detroit', temperature: 48.0 },
+      { name: 'New York', temperature: 80.0 }
+    ]))
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "cityNames" which takes an array of city objects like the
 // above problem and returns an array of the cities names.
+
+function cityNames(cities){
+    let cityNamesArray = []
+    for(let i=0; i< cities.length; i++){
+        cityNamesArray.push(cities[i].name)
+    }
+    return cityNamesArray
+}
+
+console.log(cityNames([
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+  ]))

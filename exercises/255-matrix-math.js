3c5bfbf57908625c686d2072c460e343f630349c
sasatanikazue <sasatanikazue@sasatanikazues-MacBook-Pro.local>
2019-06-29 11:36:36 -0500

    Done 255

--- exercises/255-matrix-math.js
+++ exercises/255-matrix-math.js
@@ -4,7 +4,19 @@
 // > matrixAdd([[1, 3], [2, 4]], [[5, 2], [1, 0]])
 // [ [ 6, 5 ], [ 3, 4 ] ]
 
+function matrixAdd(mat1, mat2){
+    let newMatrix = []
+    for (let i = 0; i < mat1.length; i++){
+        let subSeries = []
+        for(let j=0; j < mat1[i].length; j++){
+            subSeries.push(mat1[i][j] + mat2[i][j])
+        }
+        newMatrix.push(subSeries)
+    }    
+    return newMatrix
+}
 
+console.log(matrixAdd([[1, 3], [2, 4]], [[5, 2], [1, 0]]))
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "matrixMultiply" which is given two two-dimensional arrays;
@@ -15,3 +27,22 @@
 // Example:
 // > matrixMultiply([[2, 4], [3, 4]], [[5, 2], [3, 1]])
 // [ [22,  8], [27, 10] ]
+
+function matrixMultiply(mat1, mat2){
+    let newMatrix = []
+    for (let i = 0; i < mat1.length; i++){
+        let subSeries = []
+        for(let j=0; j < mat1[i].length; j++){
+            console.log('-------------')
+            let element = 0
+            for(let k=0; k<mat2.length; k++){
+                element += mat1[i][k] * mat2[k][j]
+                console.log(mat1[i][k] * mat2[k][j])
+            }
+            subSeries.push(element)
+        }
+        newMatrix.push(subSeries)
+    }
+    return newMatrix
+}
+console.log(matrixMultiply([[2, 4], [3, 4]], [[5, 2], [3, 1]]))

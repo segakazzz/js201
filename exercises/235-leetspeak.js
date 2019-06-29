eba78c094c3ec3ee481f5510e6c8790dfc964dcd
sasatanikazue <sasatanikazue@sasatanikazues-MacBook-Pro.local>
2019-06-28 21:12:54 -0500

    Done 235

--- exercises/235-leetspeak.js
+++ exercises/235-leetspeak.js
@@ -14,3 +14,29 @@
 // Examples:
 // leetspeak('Leet') --> "l337"
 // leetspeak('ORANGE') --> "0r4n63"
+
+
+function leetspeak(string){
+    const obj = {
+        A: 4,
+        E: 3,
+        G: 6,
+        I: 1,
+        O: 0,
+        S: 5,
+        T: 7
+    }
+    let newString = ''
+    for (let i=0; i< string.length; i++){
+        //console.log(obj[string[i].toUpperCase()])
+        if(obj[string[i].toUpperCase()] !== undefined){
+            newString += obj[string[i].toUpperCase()]
+        } else {
+            newString += string[i].toLowerCase()
+        }
+    }
+    return newString
+
+}
+console.log(leetspeak('Leet'))
+console.log(leetspeak('ORANGE'))
